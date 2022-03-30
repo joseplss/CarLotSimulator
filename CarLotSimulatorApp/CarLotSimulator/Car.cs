d18b1bf14307188d2e77614afd0ff748ed69600b
Jose <josepls.v@hotmail.com>
2022-03-30 12:12:58 -0700

    first commit

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,36 @@
+﻿namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public Car(int year, string make, string model, string engineNoise, string honkNoise, bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDrivable = isDrivable;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDrivable { get; set; }
+
+        public void MakeEngineNoise()
+        {
+            System.Console.WriteLine(EngineNoise);
+        }
+
+        public void MakeHonkNoise()
+        {
+            System.Console.WriteLine(HonkNoise);
+        }
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -19,15 +19,48 @@ namespace CarLotSimulator
             //Call each of the methods for each car
 
             //*************BONUS*************//
+            var carLot = new CarLot();
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
+            var mazda = new Car();
+            mazda.Make = "Mazda";
+            mazda.Model = "CX-5";
+            mazda.Year = 2016;
+            mazda.IsDrivable = true;
+            mazda.EngineNoise = "Zoom zoom";
+            mazda.HonkNoise = "beeeep!";
 
+            carLot.ParkingLot.Add(mazda);
+
+            //object initializer syntax
+            var ford = new Car()
+            {
+                Make = "Ford",
+                Model = "Focus",
+                Year = 2005,
+                IsDrivable = false,
+                EngineNoise = "shake shake shake die",
+                HonkNoise = "beep"
+            };
+            carLot.ParkingLot.Add(ford);
+
+            //custome constructor
+            var jeep = new Car(2020, "Jeep", "Wrangler Unlimited", "vroom!", "HONK!", true);
+            carLot.ParkingLot.Add(jeep);
+            
             //*************BONUS X 2*************//
 
             //Create a CarLot class
-            //It should have at least one property: a List of cars
-            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
+            //It should have at least one property: a List of cars - done
+            //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list. - done
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            foreach (var eachCar in carLot.ParkingLot)
+            {
+                Console.WriteLine($"{eachCar.Year} {eachCar.Make} {eachCar.Model}");
+                eachCar.MakeEngineNoise();
+                eachCar.MakeHonkNoise();
+                Console.WriteLine("-------------");
+            }
         }
     }
 }
